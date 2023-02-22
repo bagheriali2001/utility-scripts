67fcb2ddd31e73a1454a94ac8ee9d65313056711
Ali Bagheri <alibagheri.1379.1389@gmail.com>
2023-02-22 15:16:48 +0330

    updating git-branch-pruner

--- git-branch-pruner/script.js
+++ git-branch-pruner/script.js
@@ -19,21 +19,24 @@ if(typeof args[5] != "string"){
 }
 
 let regexString = "";
+let pipeString = "";
 args[5].split(",").forEach((item) => {
 	regexString += `^[ *]*${item.trim()}$|`;
+	pipeString += `${item.trim()},`;
 });
 regexString = regexString.slice(0, regexString.length - 1);
+pipeString = pipeString.slice(0, pipeString.length - 1);
 
 let command = "";
 if(args[3] == "windows"){
 	for(let i = 0 ; i < count; i++){
-		command += `cd "${ args[6 + i] }"; git remote update origin --prune; git branch --merged ${args[4]} | ForEach-Object {echo $_; if($_ | Select-String -Pattern "${regexString}"){echo nope} else {git branch $_.Trim() -d}};`
+		command += `cd "${ args[6 + i] }"; git remote update origin --prune; git checkout ${args[4]}; git branch --merged ${args[4]} | ForEach-Object {echo $_; if($_ | Select-String -Pattern "${regexString}"){echo nope} else {git branch $_.Trim() -d}};`
 	}
 	fs.writeFileSync('./script.ps1', command);
 }
 else if(args[3] == "linux"){
 	for(let i = 0 ; i < count; i++){
-		command += `cd "${ args[6 + i] }"; git remote update origin --prune; git branch --merged ${args[4]} | grep -v '${regexString}' | xargs git branch -d;`
+		command += `cd "${ args[6 + i] }"; git remote update origin --prune; git checkout ${args[4]}; git branch --merged ${args[4]} | grep -v -E "(${pipeString})" | xargs git branch -d;`
 	}
 	fs.writeFileSync('./script.sh', command);
 }
